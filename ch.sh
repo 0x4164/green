227eb68bf726106f8bd7cf29e4f0b30d07cf12a2
0x4164 <adib35785@gmail.com>
2020-01-01 09:17:00 +0700

    test 11

--- ch.sh
+++ ch.sh
@@ -1,7 +1,7 @@
 for i in $(seq 1 9); do echo $i;
 	for j in $(seq 1 28); do echo $j;
 	echo "2020"$i$j" 09:17:00"
-	sudo date -s "2020-10-$i 09:17:00";
+	sudo date -s "2020-$i-$j 09:17:00";
 	date;
 	head -c 10 /dev/urandom | xxd -p | tee test;
 	git add .;
